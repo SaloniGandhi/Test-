75a2f23a627e20e695efcbf384111e5b1b9a4830
Salonig97 <salonig97@gmail.com>
2017-09-26 20:19:32 +0530

    Update mod1_test.py

--- mod1_test.py
+++ mod1_test.py
@@ -1,8 +1,8 @@
 import unittest
 class ForTwitter(unittest.TestCase):
 	def setUp(self):
-		print ("hello testing travis")
+		print 'hello testing travis'
 	def leaving(self):
-		print ("Exiting Travis")
+		print 'Exiting Travis'
 if __name__ == "__main__"
 	unittest.main()
