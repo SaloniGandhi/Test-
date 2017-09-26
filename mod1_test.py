863e97c0c115d17270eb27d6db608876aaeac6f6
Saloni Gandhi <salonig97gmai.com>
2017-09-26 20:12:25 +0530

    mod1_test.py

---
+++ mod1_test.py
@@ -0,0 +1,8 @@
+import unittest
+class ForTwitter(unittest.TestCase):
+	def setUp(self):
+		print "hello testing travis"
+	def leaving(self):
+		print "Exiting Travis"
+if __name__ == "__main__";
+	unittest.main()
