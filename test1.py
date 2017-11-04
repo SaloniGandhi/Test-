5bebb4465879013c6deb3d8f29db69c77f627f52
SMG_97 <salonig97@gmail.com>
2017-11-05 00:49:37 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,9 @@ from twitter_client import get_twitter_auth
 import unittest
 
 class test_client_auth(unittest.TestCase):
-	def verify():
+name	def verify():
 		self.assertEqual(consumer_key,"fJriwU9JcNHkzAJYJ7PoI7ftH","error")
 		
-	
+if __name__ == "__main__":
+	unittest.main()
+	print('hello')
