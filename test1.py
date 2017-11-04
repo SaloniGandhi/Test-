068c6ab6e828dfec199d948eaea7d914a61376ee
SMG_97 <salonig97@gmail.com>
2017-11-05 01:00:48 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,8 +2,12 @@ from twitter_client import get_twitter_auth
 import unittest
 
 class test_client_auth(unittest.TestCase):
+	def setUp(self):
+		print ('hello testing travis')
 	def verify():
 		self.assertIsNot(consumer_key,"fJriwU9JcNHkzAJYJ7PoI7ftH","error")
+	def leaving(self):
+		print ('Exiting Travis')
 		
 if __name__ == "__main__":
 	unittest.main()
