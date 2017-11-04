129add149a8794e3b52296af5903eb969fce01b8
SMG_97 <salonig97@gmail.com>
2017-11-05 00:05:26 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+from twitter_client.py import get_twitter_auth
+import unittest
+
+class test_client_auth(unittest.TestCase):
+	def verify():
+		self.assertEqual(consumer_key,"fJriwU9JcNHkzAJYJ7PoI7ftH")
+		
+	
