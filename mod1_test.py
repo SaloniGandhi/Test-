a1c9bcaa2e2893a276774f6b38a35303400fee6f
SMG_97 <salonig97@gmail.com>
2017-11-06 13:58:42 +0530

    Delete mod1_test.py

--- mod1_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-class ForTwitter(unittest.TestCase):
-	def setUp(self):
-		print ('hello testing travis')
-	def leaving(self):
-		print ('Exiting Travis')
-if __name__ == "__main__":
-	unittest.main()
-	print('hello')
